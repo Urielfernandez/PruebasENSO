ba762c2d68433126f6daa103b4dd81b44ccf24d8
AndreCampos5 <andre.campos@rai.usc.es>
2020-05-18 11:15:13 +0200

    Pruebas ejecutadas

---
+++ PruebasENSO/test/AnalisisYEstadisticas/TestVerPuntuacionesPlatosDia.java
@@ -0,0 +1,14 @@
+package AnalisisYEstadisticas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestVerPuntuacionesPlatosDia {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
