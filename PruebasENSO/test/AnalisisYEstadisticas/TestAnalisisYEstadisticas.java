5daf8f76103ac830c420c313a66aefa1117ebac8
Urielfernandez <urielfernandezgranha@gmail.com>
2020-05-19 18:34:21 +0200

    Formato de las pruebas arreglado

--- PruebasENSO/test/AnalisisYEstadisticas/TestAnalisisYEstadisticas.java
+++ PruebasENSO/test/AnalisisYEstadisticas/TestAnalisisYEstadisticas.java
@@ -65,28 +65,18 @@ class TestAnalisisYEstadisticas {
 		@DisplayName("PCB-04-C1")
 		void PR05_CP001(){
 			//Arrange
-
-			//Act
-
 			HashMap<Date,Date> tiempos = new HashMap<>();
 			Date fecha = new Date();
+			HashMap<Integer, Double> x = null;
+			//Act
+
+
 			tiempos.put(fecha, fecha);
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
-				
-			} catch (IOException | JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			HashMap<Integer, Double> x = null;
-			try {
 				x = this.ae.verHorasMasConcurridas();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
+			} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -99,32 +89,23 @@ class TestAnalisisYEstadisticas {
 		@DisplayName("PCB-04-C2")
 		void PR05_CP002(){
 			//Arrange
-
-			//Act
-
 			HashMap<Date,Date> tiempos = new HashMap<>();
 			Date fecha = new Date();
+			HashMap<Integer, Double> x = null;
+			//Act
+
+
 			tiempos.put(fecha, fecha);
 			tiempos.put(fecha, fecha);
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
-				
+				x = this.ae.verHorasMasConcurridas();
 			} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			HashMap<Integer, Double> x = null;
-			try {
-				x = this.ae.verHorasMasConcurridas();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			
 			//Assert
 			assertNotNull(x,"No se han obtenido horas más concurridas");
@@ -146,30 +127,20 @@ class TestAnalisisYEstadisticas {
 		@DisplayName("PCB-04-C4")
 		void PR05_CP004(){
 			//Arrange
-
-			//Act
-
 			HashMap<Date,Date> tiempos = new HashMap<>();
 			Date fecha = new Date();
 			Date fecha2 = new Date(1000000);
+			HashMap<Integer, Double> x = null;
+			//Act
+
+			
 			tiempos.put(fecha, fecha);
 			tiempos.put(fecha2,fecha2);
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
-				
-			} catch (IOException | JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			HashMap<Integer, Double> x = null;
-			try {
 				x = this.ae.verHorasMasConcurridas();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
+			} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -182,11 +153,8 @@ class TestAnalisisYEstadisticas {
 		@DisplayName("PCB-04-C5")
 		void PR05_CP005(){
 			//Arrange
-
-			//Act
-
 			HashMap<Date,Date> tiempos = new HashMap<>();
-			
+			//Act
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
@@ -206,68 +174,50 @@ class TestAnalisisYEstadisticas {
 		@DisplayName("PCB-04-C6")
 		@Disabled("El camino asociado a 8 -> 10 es instratable")
 		void PR05_CP006(){
-			//Arrange
-
-			//Act
-
+			/*//Arrange
 			HashMap<Date,Date> tiempos = new HashMap<>();
 			Date fecha = new Date(0);
+			HashMap<Integer, Double> x = null;
+			//Act
+
+
 			tiempos.put(fecha, fecha);
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
-				
+				x = this.ae.verHorasMasConcurridas();
 			} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			HashMap<Integer, Double> x = null;
-			try {
-				x = this.ae.verHorasMasConcurridas();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
 			
 			//Assert
-			assertNotNull(x,"No se han obtenido horas más concurridas");
+			assertNotNull(x,"No se han obtenido horas más concurridas");*/
 		}
 		
 		@Test
 		@DisplayName("PCB-04-C7")
 		void PR05_CP007(){
 			//Arrange
-
-			//Act
-
 			HashMap<Date,Date> tiempos = new HashMap<>();
 			Date fecha = new Date(3000000);
 			Date fecha2 = new Date(1000000);
+			HashMap<Integer, Double> x = null;
+			//Act
+
+			
 			tiempos.put(fecha, fecha);
 			tiempos.put(fecha2,fecha2);
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
-				
+				x = this.ae.verHorasMasConcurridas();
 			} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			HashMap<Integer, Double> x = null;
-			try {
-				x = this.ae.verHorasMasConcurridas();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			
 			//Assert
 			assertNotNull(x,"No se han obtenido horas más concurridas");
@@ -277,29 +227,19 @@ class TestAnalisisYEstadisticas {
 		@DisplayName("PCB-04-C8")
 		void PR05_CP008(){
 			//Arrange
-
+			HashMap<Date,Date> tiempos = new HashMap<>();
+			HashMap<Integer, Double> x = null;
+			Date fecha = new Date();
 			//Act
 
-			HashMap<Date,Date> tiempos = new HashMap<>();
-			Date fecha = new Date();
+			
 			tiempos.put(fecha, fecha);
 			tiempos.put(fecha, fecha);
 
 			try {
 				Mockito.when(mockDatosFacturas.obtenerParesFechas()).thenReturn(tiempos);
-				
-			} catch (IOException | JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			HashMap<Integer, Double> x = null;
-			try {
 				x = this.ae.verHorasMasConcurridas();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
+			} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
